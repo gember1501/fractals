752a3e2b5dd6e9c162aa3522948cb792272b9095
gember1501 <ebehoekema@gmail.com>
2023-02-15 20:22:13 +0100

    trying something out

--- newfractals.py
+++ newfractals.py
@@ -1,6 +1,11 @@
 import cairo 
+
+def huisje(x, y, grootte):
+
+    context.rectangle(x, y, grootte, grootte)
+
 with cairo.SVGSurface("fractal.svg", 700, 700) as surface:
     context = cairo.Context(surface)
-
+huisje(100, 100, 100)
 
 print("File Saved")
