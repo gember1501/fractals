db7dc1dc6677c759bb0adc1dba2fa567c6e94ad5
gember1501 <ebehoekema@gmail.com>
2023-02-15 20:23:28 +0100

    trying somethin out

--- newfractals.py
+++ newfractals.py
@@ -4,7 +4,7 @@ def huisje(x, y, grootte):
 
     context.rectangle(x, y, grootte, grootte)
 
-with cairo.SVGSurface("fractal.svg", 700, 700) as surface:
+with cairo.SVGSurface("newfractal.svg", 700, 700) as surface:
     context = cairo.Context(surface)
 huisje(100, 100, 100)
 
