cd395f10b9ae2f7ce203bc76e802b9d7a36ed61f
gember1501 <ebehoekema@gmail.com>
2023-02-16 20:39:23 +0100

    making my first house in pycairo

--- newfractals.py
+++ newfractals.py
@@ -1,9 +1,27 @@
 import cairo 
+import math as m
 
 
+
+def triangle(ctx, lengte, x, y):
+    rechts = x + lengte
+    midden = x + (lengte * 0.5) 
+    hoogte = y - (lengte * 0.5) 
+
+
+    ctx.move_to(x, y)
+    ctx.line_to(rechts, y)
+    ctx.line_to(midden, hoogte)
+    ctx.close_path()
+    
+    
 def huisje(ctx, x, y, grootte):
 
+
     ctx.rectangle(x, y, grootte, grootte)
+    triangle(ctx, grootte, x, y)
+    
+    
 
 with cairo.SVGSurface("newfractal.svg", 700, 700) as surface:
     context = cairo.Context(surface)
