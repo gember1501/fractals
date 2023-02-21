cb0b4b02ef8c4b7aca6a37951bd1dcd9b427e022
gember1501 <ebehoekema@gmail.com>
2023-02-21 20:07:44 +0100

    just making the first fractal in pycairo

--- newfractals.py
+++ newfractals.py
@@ -15,18 +15,37 @@ def triangle(ctx, lengte, x, y):
     ctx.close_path()
     
     
-def huisje(ctx, x, y, grootte):
+def huisje(ctx, x, y, grootte, hoek, aantal):
 
 
-    ctx.rectangle(x, y, grootte, grootte)
-    triangle(ctx, grootte, x, y)
+    # ctx.rectangle(x, y, 3, 3)
+    ctx.translate(x, y )
+    ctx.rotate(hoek*m.pi/180)
+    context.set_source_rgba(0, 0, 0, 1)
+
+
+    ctx.rectangle(0, 0 - grootte, grootte, grootte)
+    triangle(ctx, grootte, 0, 0 - grootte)
+
+    if aantal > 0:
+        grootte_nw = nieuwe_lengte(grootte)
+        aantal_nw = aantal - 1
+        huisje(ctx, 0, 0-grootte, grootte_nw, 315, aantal_nw)
+        #huisje(ctx, 0, 0-grootte, grootte_nw, 45, aantal_nw)
     
-    
+
+def nieuwe_lengte (huidige_lengte):
+    # pythagoraske
+    halve_lengte = huidige_lengte * 0.5    
+    te_wortelen = (halve_lengte * halve_lengte) + (halve_lengte * halve_lengte)
+
+    return m.sqrt(te_wortelen)
+
 
 with cairo.SVGSurface("newfractal.svg", 700, 700) as surface:
     context = cairo.Context(surface)
     context.set_source_rgba(0, 0, 0, 1)
-    huisje(context, 600, 400, 100)
+    huisje(context, 450, 400, 100, 0, 5)
     context.stroke()
 
 print("File Saved")
