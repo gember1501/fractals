a4d805d4e4a7a8fd720180adf44bcebabce94620
gember1501 <ebehoekema@gmail.com>
2023-03-04 10:42:50 +0100

    does'nt work yet

--- newfractals.py
+++ newfractals.py
@@ -3,42 +3,43 @@ import math as m
 
 
 
-def triangle(ctx, lengte, x, y):
+def triangle(lengte, x, y):
     rechts = x + lengte
     midden = x + (lengte * 0.5) 
     hoogte = y - (lengte * 0.5) 
 
 
-    ctx.move_to(x, y)
-    ctx.line_to(rechts, y)
-    ctx.line_to(midden, hoogte)
-    ctx.close_path()
+    context.move_to(x, y)
+    context.line_to(rechts, y)
+    context.line_to(midden, hoogte)
+    context.close_path()
     
     
-def huisje(ctx, x, y, grootte, hoek, aantal):
+def huisje(x, y, grootte, hoek, aantal):
 
 
     # ctx.rectangle(x, y, 3, 3)
     
-    if hoek == 315:
-        ctx.translate(x, y )
-        ctx.rotate(hoek*m.pi/180)
-        ctx.set_source_rgba(0, 0, 0, 1)
-        ctx.rectangle(0, 0 - grootte, grootte, grootte)
-        triangle(ctx, grootte, 0, 0 - grootte)
+    if hoek ==315 or aantal == 7:
+        context.translate(x, y )
+        context.rotate(hoek*m.pi/180)
+        context.set_source_rgba(0, 0, 0, 1)
+        context.rectangle(0, 0 - grootte, grootte, grootte)
+        triangle(grootte, 0, 0 - grootte)
 
     else:
-        ctx.translate(x, y)
-        ctx.translate(hoek*m.pi/180)
-        ctx.set_source_rgba(0, 0, 0, 1)
-        ctx.rectangle(0, 0, grootte, grootte)
+        context.translate(x, y)
+        context.rotate(hoek*m.pi/180)
+        context.set_source_rgba(0, 0, 0, 1)
+        context.rectangle(0, 0, grootte, grootte)
+        triangle(grootte, 0, 0 - grootte)
 
 
     if aantal > 0:
             grootte_nw = nieuwe_lengte(grootte)
-            aantal_nw = aantal - 1
-            huisje(ctx, 0, 0-grootte, grootte_nw, 315, aantal_nw)
-            #huisje(ctx, 0, 0-grootte, grootte_nw, 45, aantal_nw)
+            aantal -= 1
+            huisje(0, 0-grootte, grootte_nw, 315, aantal)
+            huisje(0, 0-grootte, grootte_nw, 45, aantal)
         
         
     
@@ -54,7 +55,7 @@ def nieuwe_lengte (huidige_lengte):
 with cairo.SVGSurface("newfractal.svg", 700, 700) as surface:
     context = cairo.Context(surface)
     context.set_source_rgba(0, 0, 0, 1)
-    huisje(context, 450, 400, 100, 0, 5)
+    huisje(400, 600, 100, 0, 7)
     context.stroke()
 
 print("File Saved")
